2d5622ec7cc2cad829aaf6ef9e47c23ff94c8281
angkrnia <anggakurnia712@gmail.com>
2025-05-17 14:42:04 +0700

    test every mnts

---
+++ Kernel.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * Define the application's command schedule.
+     */
+    protected function schedule(Schedule $schedule): void
+    {
+        // $schedule->command('inspire')->hourly();
+        $schedule->command('backup:db')
+            ->everyMinute()
+            ->when(function () {
+                $hourUtc = (int) now()->format('H'); // jam UTC
+
+                // cek jam UTC antara 1 sampai 16
+                return $hourUtc >= 1 && $hourUtc <= 16;
+            });
+    }
+
+    /**
+     * Register the commands for the application.
+     */
+    protected function commands(): void
+    {
+        $this->load(__DIR__.'/Commands');
+
+        require base_path('routes/console.php');
+    }
+}
